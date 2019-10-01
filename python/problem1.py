e4deac1796274d6267541a1866c5db917e87f613
jerilMJ <jeril.0ff@gmail.com>
2019-10-01 18:18:49 +0530

    Initial commit

---
+++ python/problem1.py
@@ -0,0 +1,36 @@
+given_set = []  # Set to store given digits
+permutations_set = []
+
+# Function to find all permutations of given set and store them in permutations set
+# This is done recursively
+def permutations(g_set, index, size):
+    if index == size:
+        perm = g_set[:]
+        for i in range(size):
+            perm[i] *= 1*(10**i)
+        permutations_set.append(sum(perm))
+    else:
+        for i in range(index, size):
+            temp = g_set[i]
+            g_set[i] = g_set[index]
+            g_set[index] = temp
+
+            permutations(g_set, index + 1, size)    # Recursive call
+
+            temp = g_set[i]
+            g_set[i] = g_set[index]
+            g_set[index] = temp
+
+
+n1, n2 = input().split(' ')
+list1 = list(n1)
+list1 = list(map(int, list1))   # Separate each digits of number 1
+
+permutations(list1, 0, len(list1))  # Find permutation of digits of number 1
+print(permutations_set)
+
+filtered = filter(lambda x: x > int(n2), permutations_set)  # Filtering numbers < number 2
+filtered = sorted(filtered) # After sorting, first item will be smallest
+
+print(filtered[0])
+
